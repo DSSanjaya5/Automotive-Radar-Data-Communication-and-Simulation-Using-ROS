4f107adafa69c180a756beff0237782980bda7fa
DSSanjaya <83571032+DSSanjaya@users.noreply.github.com>
2023-10-16 22:12:21 +0530

    Create pc1.cpp

---
+++ pc1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+#include "hackathon/location.h"
+#include "hackathon/radar_data.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "pc1");
+
+  ros::NodeHandle n;
+
+  ros::Publisher RadarData_pub = n.advertise<hackathon::radar_data>("RadarData", 1000);
+
+  ros::Rate loop_rate(1.5);
+  
+  default_random_engine gen;
+  uniform_real_distribution<double>  gen_azimuth(-180.0, 180.0);
+  uniform_real_distribution<double>  gen_elevation(0.0, 180.0);
+  //uniform_real_distribution<double>  gen_radius(0.0, 100.0);
+  
+  while (ros::ok())
+  { 
+    hackathon::location loc;
+    hackathon::radar_data data;
+    
+    for(int i=0; i<6; i++)
+    {
+      loc.radius = 9;
+      loc.azimuthAngle = gen_azimuth(gen);
+      loc.elevationAngle = gen_elevation(gen);
+      data.location_list.push_back(loc);
+      ROS_INFO_STREAM("R = "<<loc.radius<<", "<<"A = "<<loc.azimuthAngle<<", "<<"E = "<<loc.elevationAngle<<endl);
+    }
+    
+    RadarData_pub.publish(data);
+    ros::spinOnce();
+    loop_rate.sleep();
+    //break;
+  }
+
+  return 0;
+}
