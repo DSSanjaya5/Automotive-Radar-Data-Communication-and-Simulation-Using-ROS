ab33a88a3c18647e4f3e6e422fa3e6f74414f7a4
DSSanjaya <83571032+DSSanjaya@users.noreply.github.com>
2023-10-16 23:04:33 +0530

    Update pc1.cpp

--- pc1.cpp
+++ pc1.cpp
@@ -8,37 +8,38 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "pc1");      // Initialization if the ROS Node
+  ros::init(argc, argv, "pc1");                  // Initialization if the ROS Node
 
-  ros::NodeHandle n;                 // Declaration of the NodeHandle
+  ros::NodeHandle n;                             // Declaration of the NodeHandle
 
-  ros::Publisher RadarData_pub = n.advertise<hackathon::radar_data>("RadarData", 1000);        // Advertising the Publisher Node
-
-  ros::Rate loop_rate(1.5);          // Setting the publishing rate = 1.5 Hz
+  ros::Publisher RadarData_pub = n.advertise<hackathon::radar_data>("RadarData", 1000);        // Advertising the Publisher Node publishing over topic - "RadarData" 
   
+  ros::Rate loop_rate(1.5);                      // Setting the publishing rate = 1.5 Hz
+
+  // Declaration of Random Data Generators
   default_random_engine gen;
   uniform_real_distribution<double>  gen_azimuth(-180.0, 180.0);
   uniform_real_distribution<double>  gen_elevation(0.0, 180.0);
-  //uniform_real_distribution<double>  gen_radius(0.0, 100.0);
-  
+  uniform_real_distribution<double>  gen_radius(0.0, 100.0);
+
+  // While ROS Node is running
   while (ros::ok())
   { 
-    hackathon::location loc;
-    hackathon::radar_data data;
+    hackathon::location loc;                      // Instance of location.msg
+    hackathon::radar_data data;                   // Instance of radar_data.msg
     
-    for(int i=0; i<6; i++)
+    for(int i=0; i<6; i++)                        // Loop for Number_Of_Locations = 6
     {
-      loc.radius = 9;
-      loc.azimuthAngle = gen_azimuth(gen);
-      loc.elevationAngle = gen_elevation(gen);
-      data.location_list.push_back(loc);
+      loc.radius = 9;                             // Set Radius
+      loc.azimuthAngle = gen_azimuth(gen);        // Set Azimuth Angle
+      loc.elevationAngle = gen_elevation(gen);    // Set Elevation Angle
+      data.location_list.push_back(loc);          // Store the values into data
       ROS_INFO_STREAM("R = "<<loc.radius<<", "<<"A = "<<loc.azimuthAngle<<", "<<"E = "<<loc.elevationAngle<<endl);
     }
     
-    RadarData_pub.publish(data);
-    ros::spinOnce();
-    loop_rate.sleep();
+    RadarData_pub.publish(data);                  // Publish the data
+    ros::spinOnce();                              // Will call all the callbacks waiting to be called at that point in time.
+    loop_rate.sleep();                            // Dynamically maintains the publishing rate 
   }
-
   return 0;
 }
